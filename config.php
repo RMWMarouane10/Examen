5288abaa75a7fc673af7321dfe17b1d3085a5907
RMWMarouane10 <136346958+RMWMarouane10@users.noreply.github.com>
2023-07-11 22:39:40 +0000

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$servername = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'dina';
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die('Échec de la connexion à la base de données : ' . $conn->connect_error);
+}
+?>
